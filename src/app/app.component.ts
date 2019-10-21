3a3092b62790c305f9699e169363dd572a9022ad
Tony Angelopoulos <louis.angelopoulos@accenturefederal.com>
2019-10-21 15:57:59 -0500

    Fixed readme and git ignore

---
+++ src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class AppComponent {
+  title = 'themeapp';
+
+  theme = new BehaviorSubject<string>('light-theme');
+
+  themes = ['light-theme', 'dark-theme', 'pink-theme'];
+
+  changeTheme(themeValue) {
+    this.theme.next(themeValue.value);
+    console.log(themeValue.value);
+  }
+}
+
